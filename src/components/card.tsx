aa1aac242c02b5aca87b7b71b36016552185666f
TelaTela <40137857+TelaTela@users.noreply.github.com>
2026-08-21 17:05:35 +0700

    rearrange layout

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -5,7 +5,7 @@ function Card({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       className={cn(
-        "border-dark-border relative flex flex-col rounded-xl border-2 py-6",
+        "border-dark-border relative flex flex-col rounded-xl border-2 px-4 py-6",
         className,
       )}
       {...props}
@@ -17,7 +17,7 @@ function CardTitle({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       className={cn(
-        "bg-dark-bg text-dark-accent-amber absolute -top-1/4 left-3 px-1 text-xl leading-none font-semibold",
+        "bg-dark-bg text-dark-accent-amber absolute -top-3.5 left-3 px-1 text-xl leading-none font-semibold",
         className,
       )}
       {...props}
