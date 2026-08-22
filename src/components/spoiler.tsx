a66f55a6a6072bc969d4c6c30d0ebfaca2e27d2d
TelaTela <40137857+TelaTela@users.noreply.github.com>
2026-08-22 23:20:48 +0700

    make spoiler component

---
+++ src/components/spoiler.tsx
@@ -0,0 +1,63 @@
+import { useEffect, useRef, useState } from "react";
+
+interface SpoilerProps {
+  spoilerText: string;
+  duration?: number;
+  intervalsBetween?: number;
+}
+
+const scrambleText = (text: string) => {
+  const chars = "░▒▓█▄▀■";
+  return text
+    .split("")
+    .map((char) =>
+      char === " " ? " " : chars[Math.floor(Math.random() * chars.length)],
+    )
+    .join("");
+};
+
+export default function ({
+  spoilerText,
+  duration,
+  intervalsBetween,
+}: SpoilerProps) {
+  const [text, setText] = useState<string>("");
+  const [restored, setRestored] = useState<boolean>(false);
+  const intervalRef = useRef<ReturnType<typeof setInterval>>(null);
+
+  if (!intervalsBetween) intervalsBetween = 30;
+  if (!duration) duration = 500 * intervalsBetween;
+
+  const scrambler = () => {
+    intervalRef.current = setInterval(() => {
+      setText(scrambleText(spoilerText));
+    }, intervalsBetween);
+  };
+
+  const restorer = () => {
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+    }
+
+    setRestored(true);
+    setText(spoilerText);
+  };
+
+  useEffect(() => {
+    scrambler();
+
+    return () => {
+      if (intervalRef.current) clearInterval(intervalRef.current);
+    };
+  }, []);
+
+  return (
+    <span
+      onClick={restorer}
+      data-restored={restored}
+      className="hover:cursor-pointer data-[restored=true]:hover:cursor-default"
+    >
+      {text}
+    </span>
+  );
+}
